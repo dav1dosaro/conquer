3ae41627850b0cdcb0cf5ce003095085e8de8979
ogeeice <ogeeicebk@gmail.com>
2022-09-14 14:15:32 -0700

    SkeletonEnemy3

--- src/scenes/enemy/skeleton/Skeleton.gd
+++ src/scenes/enemy/skeleton/Skeleton.gd
@@ -1,19 +1,23 @@
 extends KinematicBody2D
 
+var PlayerLoc
+export var Player_Path : NodePath
+
 var gravity = 10
 var velocity = Vector2(0,0)
 var Moving_Right = true
 
 export var Speed = 32
-export var Health = 1
+export var Health = 3
 export var Enemy_Attack_damage = 1
 export var Wall_Smarts = false
-export var Push_Back_Force = 1.0
+export var Attack_Range = 100
 
 onready var Floor_Raycast = get_node("Floor_Ray")
 onready var Wall_Raycast = get_node("Wall_Ray")
 
 func _ready():
+	PlayerLoc = get_node(Player_Path)
 	if Wall_Smarts == true:
 		Wall_Raycast.enabled = true
 	else:
@@ -23,6 +27,7 @@ func _process(_delta):
 	Health = clamp(Health,0,1)
 	Move()
 	Detect_Floor()
+	Attack()
 	if Wall_Smarts == true:
 		Detect_Wall()
 
@@ -31,6 +36,7 @@ func Move():
 		velocity.x = Speed
 	else:
 		velocity.x = -Speed
+	get_node("Body").play("Walk")
 	
 	velocity.y += gravity
 	
@@ -48,9 +54,8 @@ func Detect_Wall():
 
 func Health_Detail(Enemy_Health_damage):
 	get_node("TimerHolder/HitTimer").start()
-	get_node("Body").material.set_shader_param("Flash_modifier",1)
+	get_node("Body").play("Damage")
 	Health -= Enemy_Health_damage
-	global_position -= Vector2(velocity.x,velocity.y) * Push_Back_Force
 	Die()
 
 func Die():
@@ -67,7 +72,6 @@ func _on_Body_animation_finished():
 		for H in get_node("HitArea").get_overlapping_bodies():
 			H.Health_Detail(Enemy_Attack_damage)
 			get_node("Attack").play()
-			get_node("TimerHolder/AttackTimer").start()
 	
 	if get_node("Body").animation == "Death":
 		self.global_position = self.global_position
@@ -80,14 +84,13 @@ func _on_Body_animation_finished():
 func _on_DeathTimer_timeout():
 	queue_free()
 
-
 func _on_HitArea_body_entered(body):
 	if body.is_in_group("PLAYER"):
+		print("DAMAGE_HIM")
+
+func Attack():
+	var PLoc = PlayerLoc.position
+	var Dis2 = self.position.distance_to(PLoc)
+	if Dis2 < Attack_Range:
 		get_node("Body").play("Attack")
-
-func _on_HitArea_body_exited(body):
-	if body.is_in_group("PLAYER"):
-		get_node("TimerHolder/AttackTimer").start()
-
-func _on_AttackTimer_timeout():
-	queue_free()
+		pass
