43be16d0717503e9e9c6fefa542e2e9f1e8d65b7
ogeeice <ogeeicebk@gmail.com>
2022-09-15 12:33:52 -0700

    EnemyBlob

--- src/scenes/enemy/Blob/Blob.gd
+++ src/scenes/enemy/Blob/Blob.gd
@@ -1,1 +1,19 @@
 extends KinematicBody2D
+
+enum EnemyStates {IDLE,CHASE,ATTACK,DEATH}
+var States = EnemyStates.IDLE
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	if States == EnemyStates.IDLE:
+		pass
+	elif States == EnemyStates.CHASE:
+		pass
+	elif States == EnemyStates.ATTACK:
+		pass
+	elif States == EnemyStates.DEATH:
+		pass
+	pass
